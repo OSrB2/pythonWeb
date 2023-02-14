a471fb8a01005eda4bc524eeba90dd662cac8deb
Pedro <pedro_oliva@oulook.com.br>
2023-02-14 20:02:13 -0300

    feat: create route, html, config env

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask('Hello')
+app = Flask(__name__)
+
+# POSTS MOCK
+posts = [
+  {
+    'titulo': 'Post 1',
+    'texto': 'Meu primeiro post.'
+  },
+  {
+    'titulo': 'Post 2',
+    'texto': 'Meu segundo post.'
+  }
+]
 
 @app.route('/')
-def hello():
-  return 'Hello World!'
+def exibir_entradas():
+  return render_template('exibir_entradas.html', entradas=posts)
